da71de2b3246c96b34af8f1a3a3eaaca4141cb8a
immersvin <immersv@gmail.com>
2021-05-26 15:02:25 +0530

    Player Movement Done

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    Vector3 direction;// 
+    public float playerSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (direction == Vector3.forward)
+            {
+                direction = Vector3.left;
+            }
+            else
+            {
+                direction = Vector3.forward;
+            }
+        }
+        transform.Translate(direction * playerSpeed * Time.deltaTime);
     }
 }
