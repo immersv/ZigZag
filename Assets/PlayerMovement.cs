138405a963278b57418c2dcbdf7ebe50a58a4ffc
immersvin <immersv@gmail.com>
2021-06-01 17:01:54 +0530

    Score Manager Done

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
     Vector3 direction;// 
     public float playerSpeed;
     public GameObject particleEffectsPrefab;
+    [SerializeField]
+    int score = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -23,21 +25,40 @@ public class PlayerMovement : MonoBehaviour
             if (direction == Vector3.forward)
             {
                 direction = Vector3.left;
+               
             }
             else
             {
                 direction = Vector3.forward;
+                
             }
+            score = score + 1;
+            Debug.Log("Score: " + score);
         }
+
         transform.Translate(direction * playerSpeed * Time.deltaTime);
+        
+       
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Pickup")
         {
+            score = score + 5;//or score+=5
+            Debug.Log("Score: " + score);
+
             other.gameObject.SetActive(false);
             Instantiate(particleEffectsPrefab, transform.position, Quaternion.identity);
         }
     }
 }
+
+
+
+/*Task 
+ * Player Death logic
+ * To Publish the score in UI
+ * to create the end canvas animation
+ * when player is dead make active the endscreen animation  and display score,Replay options
+ */
